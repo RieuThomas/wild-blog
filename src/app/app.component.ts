c18bca70bd4669dc81909e967b92dc65cfab8f54
Rieu Thomas <rieu.thomas@hotmail.com>
2026-05-21 14:48:49 +0200

    Router construction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header.component';
-import { ArticleList } from './components/article-list/article-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, ArticleList ],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class App {
-  protected readonly title = signal('Bienvenue sur le Wild Blog de Thomas !');
-}
+export class App {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page.component';
+import { ArticlePage } from './pages/article-page/article-page.component';
+import { NotFoundPage } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component: HomePage},
+    {path:"article/:id", component: ArticlePage},
+    {path: "**", component: NotFoundPage}
+];

036436a085ed5684524476bebb3128b79d05b645
Rieu Thomas <rieu.thomas@hotmail.com>
2026-05-26 11:58:53 +0200

    signup comoonent creation  with reactive form

---
+++ src/app/components/signup-component/signup-component.component.ts
@@ -0,0 +1,55 @@
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup-component',
+  standalone:true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './signup-component.component.html',
+  styleUrl: './signup-component.component.scss',
+})
+export class SignupComponent {
+
+  formBuilder = inject(FormBuilder);
+
+    signUpForm = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    passwords: this.formBuilder.group({
+      password: ['', [Validators.required, this.securePasswordValidator()]],
+      confPassword: ['']
+    }, { validators: this.passwordMatchValidator() })
+  });
+
+    securePasswordValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value || '';
+      
+      const hasUpperCase = /[A-Z]/.test(value);
+      const hasLowerCase = /[a-z]/.test(value);
+      const hasNumber = /\d/.test(value);
+      const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+      const isValidLength = value.length >= 12;
+
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && isValidLength;
+
+      return passwordValid ? null : { securePassword: true };
+    };
+  }
+
+passwordMatchValidator(): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const password = formGroup.get('password')?.value;
+      const confPassword = formGroup.get('confPassword')?.value;
+      return password === confPassword ? null : { passwordsMismatch: true };
+    };
+  }
+
+  onSubmit(): void {
+    if (this.signUpForm.valid) {
+      console.log('Formulaire envoyé avec succès', this.signUpForm.value);
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
+}

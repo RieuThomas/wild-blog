1945524f064918c6b008f245e1430507d0d5e83a
Rieu Thomas <rieu.thomas@hotmail.com>
2026-05-26 10:40:55 +0200

    adds of contact route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page.component';
 import { ArticlePage } from './pages/article-page/article-page.component';
 import { NotFoundPage } from './pages/not-found-page/not-found-page.component';
+import { ContactPage } from './pages/contact-page/contact-page.component';
 
 export const routes: Routes = [
-    {path:"", component: HomePage},
-    {path:"article/:id", component: ArticlePage},
+    {path: "", component: HomePage},
+    {path: "article/:id", component: ArticlePage},
+    {path: "contact", component: ContactPage},
     {path: "**", component: NotFoundPage}
 ];
